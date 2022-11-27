9af2e8f5728e60520f1d9aae85b141363e431b94
zhanibekibatov <zhanibek.99.99@mail.ru>
2022-11-27 17:50:10 +0500

    Initial commit

---
+++ src/sabak1.java
@@ -0,0 +1,11 @@
+public class sabak1 {
+    public static void main(String[] args) {
+
+        int height = 2;
+        int width = 5;
+
+        byte b = 127;
+        int rectangleArea = height * width;
+        System.out.println(rectangleArea);
+    }
+}
