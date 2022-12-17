d58385575554770ab05fb527059ef68fc2ba3d3e
zhanibekibatov <zhanibek.99.99@mail.ru>
2022-12-17 15:52:32 +0500

    java

---
+++ src/JavaAlishev/DoWhile.java
@@ -0,0 +1,18 @@
+package JavaAlishev;
+
+import java.util.Scanner;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int value;
+
+        do {
+            System.out.print("Enter the num 5 -> ");
+            value = scanner.nextInt();
+        } while(value != 5);
+
+
+        System.out.println("You typed 5");
+    }
+}
--- src/JavaAlishev/Input.java
+++ src/JavaAlishev/Input.java
@@ -1,7 +1,13 @@
 package JavaAlishev;
 
+import java.util.Scanner;
+
 public class Input {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
 
+        System.out.print("Enter any number...");
+        int x  = s.nextInt();
+        System.out.println("You typed " + x);
     }
 }
